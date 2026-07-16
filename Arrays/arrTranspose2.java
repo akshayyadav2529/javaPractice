7f721fb10790f2e16db8709bf1c88bea0d300509
Akshay Yadav <akshayyadav6342@gmail.com>
2026-07-16 21:53:03 +0545

    Solved the error function in swap program by using temp variable for a proper loop

--- Arrays/arrTranspose2.java
+++ Arrays/arrTranspose2.java
@@ -1,11 +1,25 @@
 public class arrTranspose2 {
-    public static void main(String[] args){
-        int[][] matrix= {{1,2,3},{4,5,6},{7,8,9}};
-        for(int i=0; i<matrix.length-1; i++){
-            for(int j=i+1; j<matrix[0].length; j++){
-                swap(matrix[i][j],matrix[j][i]);
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                swap(matrix, i, j);
+            }
+        }
+
+        // print the transposed matrix
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
             }
             System.out.println();
         }
-       
     }
+
+    public static void swap(int[][] matrix, int i, int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+}
